9b277c15133637957776878f39b8a5febd186a55
Taylor Lo <taylorloph@gmail.com>
2024-01-28 14:50:40 -0500

    Arrays - 06 - Merge Sorted Arrays

--- 03 - Data Structures/Arrays/06 - Merge Sorted Arrays.js
+++ 03 - Data Structures/Arrays/06 - Merge Sorted Arrays.js
@@ -5,8 +5,18 @@ function mergeSortedArrays(arr1, arr2) {
   // check input
   newArr = [];
   if (!arr1 || !arr2 || typeof arr1 !== "object" || typeof arr2 !== "object") {
-    console.log("Please input arr!")
+    console.log("Please input arr!");
   }
-  arr1.push(arr2);
-  return arr1;
+  for (let i = 0; i < arr1.length; i++) {
+    newArr.push(arr1[i]);
+  }
+  for (let i = 0; i < arr2.length; i++) {
+    newArr.push(arr2[i]);
+  }
+  for (let i = 0; i < newArr.length; i++) {
+    if (newArr[i] > newArr[i + 1]) {
+        newArr[i] = newArr[i + 1]
+    }
+  }
+  return newArr;
 }
---
+++ 03 - Data Structures/Arrays/07 - Merge Sorted Arrays Solution.js
@@ -0,0 +1,7 @@
+function mergeSortedArrays(array1, array2) {
+    const mergedArray = [];
+
+    return mergedArray
+};
+
+mergeSortedArrays([0, 3, 4, 31], [4, 6, 30]);
