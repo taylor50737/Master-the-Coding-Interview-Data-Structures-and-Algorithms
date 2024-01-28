0288be14ca140a2f8a22200673fb1cc49eb563db
Taylor Lo <taylorloph@gmail.com>
2024-01-27 21:52:20 -0500

    Big O - 08 - Big O Rule 2

--- Big O/07 - Big O Rule 1.js
+++ Big O/07 - Big O Rule 1.js
@@ -1,10 +1,10 @@
 //#1 -- For loop in Javascript.
-const nemo = ['nemo'];
+const nemo = ["nemo"];
 
 function findNemo1(array) {
   for (let i = 0; i < array.length; i++) {
-    if (array[i] === 'nemo') {
-      console.log('Found NEMO!');
+    if (array[i] === "nemo") {
+      console.log("Found NEMO!");
     }
   }
 }
