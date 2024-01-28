23daacbb0bc93ddfaa93de6e7309f96fb53b6332
Taylor Lo <taylorloph@gmail.com>
2024-01-27 20:54:46 -0500

    Big O - 06 - Big O Calculation2

--- Big O/05 - Big O Calculation.js
+++ Big O/05 - Big O Calculation.js
@@ -3,8 +3,7 @@ function funChallenge(input) {
   let a = 10; // O(1)
   a = 50 + 3; // O(1)
 
-  for (let i = 0; i < input.length; i++) {
-    // O(n)
+  for (let i = 0; i < input.length; i++) { // O(n)
     anotherFunction(); // O(n)
     let stranger = true; // o(n)
     a++; // o(n)
@@ -12,4 +11,4 @@ function funChallenge(input) {
   return a; // O(1)
 }
 
-// BIG O(n)
+// Big O = 3 + 4n = O(n)
