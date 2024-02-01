556df667e0ed1ef1e3757712128e6ba0601e16c4
Taylor Lo <taylorloph@gmail.com>
2024-01-31 22:55:01 -0500

    Trees - 02 - O(log n)

---
+++ 03 - Data Structures/05 - Trees/02 - O(log n).js
@@ -0,0 +1,13 @@
+/*
+level 0: 2^0 = 1;
+level 1: 2^1 = 2;
+level 2: 2^2 = 4;
+level 3: 2^3 = 8;
+ 
+# of nodes = 2^h - 1;
+h = height, if we find out how many level the binary tree is, we can find out how many total nodes are there
+
+log nodes = step
+log 100 = 2
+10^2 = 100;
+*/
