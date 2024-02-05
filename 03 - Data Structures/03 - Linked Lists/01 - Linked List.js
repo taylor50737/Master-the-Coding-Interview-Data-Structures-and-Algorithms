3e009f8a5d337ea838084d94f2cc38c7a50be7cf
Taylor Lo <taylorloph@gmail.com>
2024-02-04 21:10:39 -0500

    Recursions - 01 - Recursion Introduction

--- 03 - Data Structures/03 - Linked Lists/01 - Linked List.js
+++
@@ -1,9 +0,0 @@
-// Array
-const basket = ["apples", "grapes", "pears"];
-
-// linked list: apples -> grapes -> pears
-
-// apples
-//  8947  -->  grapes
-//              8742   -->  pears
-//                           372   -->  null
